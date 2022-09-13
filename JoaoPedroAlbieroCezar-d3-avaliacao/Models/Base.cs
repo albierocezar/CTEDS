221f4c488f3bed1aa1b8daacb64b3668eeafbd46
unknown <cteds_13>
2022-09-12 21:58:03 -0300

    ultimas att apos aula

--- JoaoPedroAlbieroCezar-d3-avaliacao/Models/Base.cs
+++ JoaoPedroAlbieroCezar-d3-avaliacao/Models/Base.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,8 @@ namespace JoaoPedroAlbieroCezar_d3_avaliacao.Models
         public static void Write_CSV(Usuario user)
         {
             DateTime now = DateTime.Now;
-            
+            string now_to_string = now.ToString("G", CultureInfo.GetCultureInfo("es-ES")); // dd/mm/yyyy h:m:s 
+
         }
-    }
+    }   
 }
